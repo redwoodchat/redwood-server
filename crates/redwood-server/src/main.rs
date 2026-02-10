6528e536c9010826dec1c996f356cdc332b6a1ac
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2026-02-09 20:48:32 -0800

    feat: very basic CLI parsing
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- crates/redwood-server/src/main.rs
+++ crates/redwood-server/src/main.rs
@@ -1,3 +1,20 @@
+use clap::{arg, command, value_parser};
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(
+            arg!(
+                -c --config <FILE> "The config file to read"
+            )
+            .required(false)
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches();
+
+    if let Some(config) = matches.get_one::<PathBuf>("config") {
+        println!("config: {}", config.display());
+    }
+
+    println!("Ready to run!");
 }
